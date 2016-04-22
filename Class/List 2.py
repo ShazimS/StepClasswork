76202447897633f7835fb202da6d429c518b77e1
Shazim <Shazimsalim@gmail.com>
2016-04-22 10:47:39 -0400

    Created temperature.py

---
+++ Class/List 2.py
@@ -0,0 +1,32 @@
+def main():
+    favColor()
+
+
+
+
+def favColor():
+    firstList=[]
+    secondList=[]
+    for a in range (0,1):
+
+        x=getname()
+        firstList.append(x)
+
+    for b in range (0,1):
+
+        c=getcolor()
+        secondList.append(c)
+    print(x,"favorite color is",c)
+    main()
+    E
+
+
+
+
+def getname():
+    return input("What is your name?")
+
+def getcolor():
+    return input("What is your fav color?")
+
+main()
